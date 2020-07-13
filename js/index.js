3038f4aeb4255f2c00c9696ca329381e7bf9f70f
larissanguyen <larissa.h.nguyen@gmail.com>
2020-07-13 14:34:21 -0400

    listing done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
-document.addEventListener("DOMContentLoaded", function() {});
+document.addEventListener("DOMContentLoaded", function() {
+	fetchBooks()
+});
+
+function fetchBooks() {
+	fetch('http://localhost:3000/books')
+	.then(res => res.json())
+	.then(getBooks => showBooks(getBooks))
+}
+
+function ce(element){
+	return document.createElement(element)
+}
+
+function appendBook(book) {
+	const item = ce('li')
+	item.innerText = book.title
+	var list = document.querySelector('ul#list')
+	list.appendChild(item)
+}
+
+function showBooks(books) {
+	books.forEach(book => 
+		appendBook(book)
+		) 
+}
+
