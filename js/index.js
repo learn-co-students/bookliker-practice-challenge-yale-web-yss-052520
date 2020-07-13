a0c47edba5ea4be8ab2a1ed5b79a7e71a81bc528
KalebGz <kaleb.zeray@gmail.com>
2020-07-13 15:00:14 -0400

    display books on side panel

--- js/index.js
+++ js/index.js
@@ -16,12 +16,32 @@ function appendBook(book) {
 	const item = ce('li')
 	item.innerText = book.title
 	var list = document.querySelector('ul#list')
-	list.appendChild(item)
+    list.appendChild(item)
+    return item
 }
 
 function showBooks(books) {
-	books.forEach(book => 
-		appendBook(book)
-		) 
+	books.forEach(book => {
+		let listItem = appendBook(book)
+        makeClickable(listItem, book)
+    }) 
 }
 
+function makeClickable(element, book){
+    element.addEventListener('click', () => {showPanel(book)})
+}
+
+function showPanel(book){
+    let panel = document.querySelector('#show-panel')
+
+    let h2 = ce("h2")
+    h2.innerText = book.title
+
+    let img = ce("img")
+    img.src = book.img_url
+
+    let p = ce("p")
+    p.innerText = book.description
+
+    panel.append(h2, img, p)
+}
