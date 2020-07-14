338a6b497c58636faa16cdf097451c83e9253458
alexisteh <wenhui.teh23@gmail.com>
2020-07-13 22:04:43 -0400

    Done.

--- js/index.js
+++ js/index.js
@@ -11,19 +11,16 @@ document.addEventListener("DOMContentLoaded", function() {
             addBook(book)
         })
     })
-
+    
     function addBook(book){
         const li = document.createElement('li')
         const shown = document.createElement('div')
-        shown.id = "shown" 
         const title = document.createElement('p')
         title.innerText = book.title 
-        
         shown.append(title)
 
         title.addEventListener('click', function(){
-            console.log(book)
-            let exists = document.querySelector('div #book' + book.id) 
+            let exists = document.querySelector('div#book' + book.id) 
             if (exists == null){ 
                 event.target.style.color = "red" 
                 const hidden = document.createElement('div')
@@ -58,63 +55,51 @@ document.addEventListener("DOMContentLoaded", function() {
 
                 likeButton.addEventListener('click', function(){
                     const currentLikers = book.users 
-                    const me = {"id":1, "username": 'pouros'}
+                    const me = {"id": 1, "username": 'pouros'}
                     if (currentLikers.map(user => user.id).includes(1)){
-                        console.log(currentLikers)
-                        let ind = currentLikers.indexOf(me)
-                        currentLikers.splice(ind)  
-                        let configObj = { 
-                            method: 'PATCH', 
-                            headers: {
-                                'Content-type': 'application/json',
-                                'Accept': 'application/json'
-                            },
-                            body: JSON.stringify({
-                                users: currentLikers
-                            }) 
-                        }
-    
-                        fetch("http://localhost:3000/books/" + book.id, configObj ) 
-                        .then(res => res.json())
-                        .then(book => {
-                            likeButton.innerText = '♡' 
-                            let userInList = document.querySelector('li#user-1book-' + book.id)
-                            userInList.remove() 
-                        } ) 
-
-                    }else {
+                        console.log(currentLikers) 
+                        let ind = currentLikers.indexOf(me) 
+                        currentLikers.splice(ind) 
+                    } else {
                         currentLikers.push(me) 
-                        let configObj = {
-                            method: 'PATCH', 
-                            headers: {
-                                'Content-type': 'application/json',
-                                'Accept': 'application/json'
-                            },
-                            body: JSON.stringify({
-                                users: currentLikers 
-                            })                         } 
-        
-                        fetch("http://localhost:3000/books/" + book.id, configObj ) 
-                        .then(res => res.json())
-                        .then(book => {
-                            likeButton.innerText = '♥' 
-                            let newLiker = document.createElement('li')
-                            newLiker.innerText = 'pouros '
-                            newLiker.id = 'user-1book-' + book.id  
-                            likers.append(newLiker) 
-                        })
+                    }
+                    let configObj = {
+                        method: 'PATCH', 
+                        headers: {
+                            'Content-type': 'application/json',
+                            'Accept': 'application/json'
+                        }, 
+                        body: JSON.stringify({
+                            users: currentLikers 
+                        })                         
                     } 
+                    fetch("http://localhost:3000/books/" + book.id, configObj) 
+                    .then(res => res.json() ) 
+                    .then(book => { 
+                    if (currentLikers.includes(me)) {
+                        likeButton.innerText = '♥' 
+                        let newLiker = document.createElement('li')
+                        newLiker.innerText = 'pouros' 
+                        newLiker.id = 'user-1book-' + book.id  
+                        likers.append(newLiker)
+                    } else {
+                        likeButton.innerText = '♡' 
+                        let userInList = document.querySelector('li#user-1book-' + book.id)
+                        userInList.remove() 
+                    }
+                    })
+                    
                 })
+
                 hidden.append(title, thumbnail, description, likers, likeButton) 
                 bookShow.append(hidden) 
-
-            }else {
+                
+            } else {
                 event.target.style.color = "black" 
                 exists.remove() 
-            }
-        })
-        
+            } 
+        }) 
         li.append(shown) 
-        bookList.append(li)
+        bookList.append(li) 
     }
 })
